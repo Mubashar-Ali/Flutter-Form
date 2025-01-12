23ee66230b6661b813e36e689599525d3cc4ac91
Mubashar Ali <mubasharalii1070@gmail.com>
2025-01-12 21:34:51 +0500

    form created

--- lib/main.dart
+++ lib/main.dart
@@ -12,53 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: App(),
-    );
-  }
-}
-
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  String text = 'Ali Raza';
-  bool b = false;
-  double slide = .5;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            AnimatedSwitcher(
-              duration: const Duration(seconds: 8),
-              child: Switch(
-                value: b,
-                onChanged: (value) {
-                  setState(() {
-                    b = value;
-                  });
-                },
-              ),
-            ),
-            const Divider(),
-            Slider(
-              value: slide,
-              onChanged: (value) {
-                setState(() {
-                  slide = value;
-                  print(value);
-                });
-              },
-            ),
-          ],
-        ),
-      ),
+      home: AnimatedForm()
     );
   }
 }
